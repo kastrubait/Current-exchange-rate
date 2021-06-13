7e27d09b617bd02906fcf350c266c2fc0cff2e17
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2021-06-13 15:52:31 +0300

    fix: request to the server, add page template

--- app.js
+++ app.js
@@ -2,24 +2,32 @@ import 'regenerator-runtime/runtime.js';
 import express from 'express';
 import axios from'axios';
 
-
-const BASE_URL = 'https://www.nbrb.by/api/exrates/currencies';
+import { BASE_URL } from './src/utilitis/constants.js';
 
 const app = express();
-// const port = process.env.PORT;
 const port = 4076;
 
 app.set('view engine', 'pug');
 app.set('views', './src/views');
 
-app.get('/', async (_req, res) => {
+app.use('/', async (_req, res) => {
   try {
-    const query = await axios.get(`${BASE_URL}`);
-    const exchangeRate = query.data;
-    console.log(`GET: Here's the list of exchange rate`, Array.isArray(exchangeRate));
-    res.render('index', exchangeRate);
+    const query = await axios.get(`${BASE_URL}/currencies`);
+    const currencyAll = query.data;
+    await Promise.all(currencyAll.map(async (rate) => {
+      try{
+        const result = await axios.get(`${BASE_URL}/rates/${rate.Cur_Abbreviation}?parammode=2`);
+        if (result.data) {
+          const { Date, Cur_OfficialRate } = result.data;
+          Object.assign(rate, { Date: Date, Cur_OfficialRate: Cur_OfficialRate });
+        }
+      } catch (errors) {
+        console.error(errors.message);
+      }})
+    );
+    res.render('index', { exchangeRate: currencyAll } );
   } catch (errors) {
-    console.error(errors);
+    console.error(errors.message);
   }
 });
 
---
+++ src/utilitis/constants.js
@@ -0,0 +1,3 @@
+export const BASE_URL = 'https://www.nbrb.by/api/exrates';
+export const DAILY = 0;
+export const MONTHLY = 1;
