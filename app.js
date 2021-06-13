9f25bc8d9f93a20daed87d3fbe7363dde2e1a28a
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2021-06-13 08:37:01 +0300

    feat: get data about rates

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 import 'regenerator-runtime/runtime.js';
 import express from 'express';
+import axios from'axios';
+
+
+const BASE_URL = 'https://www.nbrb.by/api/exrates/currencies';
 
 const app = express();
 // const port = process.env.PORT;
@@ -8,8 +12,15 @@ const port = 4076;
 app.set('view engine', 'pug');
 app.set('views', './src/views');
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get('/', async (_req, res) => {
+  try {
+    const query = await axios.get(`${BASE_URL}`);
+    const exchangeRate = query.data;
+    console.log(`GET: Here's the list of exchange rate`, Array.isArray(exchangeRate));
+    res.render('index', exchangeRate);
+  } catch (errors) {
+    console.error(errors);
+  }
 });
 
 app.listen(port, () => {
